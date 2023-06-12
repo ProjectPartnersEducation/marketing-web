4daa854c63dfd514137e86643998a3bbe9ba5479
Jonny Jackson <jonny@jxn.co>
2023-06-12 12:45:16 +0100

    Fix CSP?

--- svelte.config.js
+++ svelte.config.js
@@ -27,7 +27,7 @@ const config = {
 		},
 		csp: {
 			directives: {
-				'script-src': ['strict-dynamic', 'self', 'projectpartners.org', 'beta.projectpartners.org', 'res.cloudinary.com/dywyr2kco/'],
+				'script-src': ['strict-dynamic', 'self', 'projectpartners.org', 'beta.projectpartners.org', 'res.cloudinary.com'],
 				'object-src': ['none'],
 				'base-uri': ['none']
 			}
